7a582b68b791a9f709bb6d7da1584155d882e9bb
hazemati <ha245@cornell.edu>
2018-11-21 22:34:01 -0800

    update indesx.js

--- index.js
+++ index.js
@@ -127,8 +127,29 @@ function registerElements(elements, exampleName) {
         // Otherwise, un-disable inputs.
         enableInputs();
       }
+          if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
     });
   });
+// Submit the form with the token ID.
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
 
   resetButton.addEventListener('click', function(e) {
     e.preventDefault();
